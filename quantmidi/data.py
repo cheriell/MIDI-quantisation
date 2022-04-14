f6fe66b606510dda72c7fd78447460936183cb4b
cheriell <fj12342005@126.com>
2022-04-14 23:26:13 +0100

    add data augmentation

--- quantmidi/data.py
+++ quantmidi/data.py
@@ -74,7 +74,7 @@ class QuantMIDIDataset(torch.utils.data.Dataset):
             return batch_size * 4 * 500     # constantly update 500 steps per epoch
                                             # not related to training dataset size
         else:
-            return batch_size * len(self.metadata)  # valid dataset size
+            return 4 * len(self.metadata)  # valid dataset size
 
     def __getitem__(self, idx):
         # random sampling by piece
@@ -88,7 +88,8 @@ class QuantMIDIDataset(torch.utils.data.Dataset):
         beats = beats
 
         # data augmentation
-        note_sequence, beats = self.dataaug(note_sequence, beats)
+        if self.split == 'train':
+            note_sequence, beats = self.dataaug(note_sequence, beats)
 
         # sample segment and get model input & output
 
@@ -137,16 +138,63 @@ class DataAugmentation():
     def __init__(self, 
         tempo_change_prob=1.0,
         tempo_change_range=(0.8, 1.2),
-        pitch_shift_prob=0.8,
+        pitch_shift_prob=1.0,
         pitch_shift_range=(-12, 12),
-        extra_note_prob=0.2,
-        missing_note_prob=0.8,):
+        extra_note_prob=0.0,
+        missing_note_prob=1.0):
+
+        if extra_note_prob + missing_note_prob > 1.:
+            raise ValueError('extra_note_prob + missing_note_prob must be less than 1.')
         
-        pass
+        self.tempo_change_prob = tempo_change_prob
+        self.tempo_change_range = tempo_change_range
+        self.pitch_shift_prob = pitch_shift_prob
+        self.pitch_shift_range = pitch_shift_range
+        self.extra_note_prob = extra_note_prob
+        self.missing_note_prob = missing_note_prob
 
     def __call__(self, note_sequence, beats):
+        # tempo change
+        if random.random() < self.tempo_change_prob:
+            note_sequence, beats = self.tempo_change(note_sequence, beats)
+
+        # pitch shift
+        if random.random() < self.pitch_shift_prob:
+            note_sequence = self.pitch_shift(note_sequence)
+
+        # extra note or missing note
+        extra_or_missing = random.random()
+        if extra_or_missing < self.extra_note_prob:
+            note_sequence = self.extra_note(note_sequence)
+        elif extra_or_missing > 1. - self.missing_note_prob:
+            note_sequence = self.missing_note(note_sequence)
+
         return note_sequence, beats
 
+    def tempo_change(self, note_sequence, beats):
+        tempo_change_ratio = random.uniform(*self.tempo_change_range)
+        note_sequence[:,1:3] *= 1 / tempo_change_ratio
+        beats *= 1 / tempo_change_ratio
+        return note_sequence, beats
+
+    def pitch_shift(self, note_sequence):
+        shift = random.uniform(*self.pitch_shift_range)
+        note_sequence[:,0] += shift
+        return note_sequence
+
+    def extra_note(self, note_sequence):
+        return note_sequence
+
+    def missing_note(self, note_sequence):
+        # find successing concurrent notes
+        candidates = torch.diff(note_sequence[:,1]) == 0
+        # randomly select a ratio of candidates to be removed
+        ratio = random.random()
+        candidates_probs = candidates * torch.rand(len(candidates))
+        remaining = torch.cat([torch.tensor([True]), candidates_probs < (1 - ratio)])
+
+        note_sequence = note_sequence[remaining.long()]
+        return note_sequence
 
 class DataUtils():
     
