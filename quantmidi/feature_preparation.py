15c7f9e0ac9bef3b61fa5ea114960a1f9f0a5d74
cheriell <fj12342005@126.com>
2022-04-15 21:22:45 +0100

    update dataset statistics

--- quantmidi/feature_preparation.py
+++ quantmidi/feature_preparation.py
@@ -10,6 +10,8 @@ import multiprocessing as mp
 from pathos.multiprocessing import ProcessingPool as Pool
 import pretty_midi as pm
 import pickle
+import numpy as np
+import shutil
 
 from quantmidi.data import DataUtils
 
@@ -153,8 +155,8 @@ class FeaturePreparation():
     def print_statistics(self):
         self.verboseprint('INFO: Printing dataset statistics')
 
-        # ======== get dataset statistics ==========
-        # number of distinct pieces
+        # ========== number of distinct pieces ==========
+        self.verboseprint('INFO: Get number of distinct pieces')
         pieces_train = set(self.metadata[self.metadata['split'] == 'train']['piece_id'].unique())
         pieces_valid = set(self.metadata[self.metadata['split'] == 'valid']['piece_id'].unique())
         pieces_test = set(self.metadata[self.metadata['split'] == 'test']['piece_id'].unique())
@@ -164,28 +166,78 @@ class FeaturePreparation():
         n_pieces_test = len(pieces_test)
         n_pieces = n_pieces_train + n_pieces_valid + n_pieces_test
         
-        # number of performances
-        piece2perf = defaultdict(set)
-        for _, row in self.metadata.iterrows():
-            if row['source'] == 'ASAP':
-                performance = row['midi_perfm']
-            elif row['source'] == 'MAPS':
-                performance = '_'.join(row['midi_perfm'].split('/')[-1][len('MAPS_MUS-'):].split('_')[:-1])
-            elif row['source'] == 'CPM':
-                performance = row['midi_perfm'].split('/')[-1][:-4]
-            piece2perf[row['piece_id']].add(performance)
+        # =========== number of performances ===========
+        self.verboseprint('INFO: Get number of performances')
+        piece2perfm_label = defaultdict(set)
+        perfms = {'train': set(), 'valid': set(), 'test': set()}
 
-        n_perfms_train = sum(len(perfms) for piece, perfms in piece2perf.items() if piece in pieces_train)
-        n_perfms_valid = sum(len(perfms) for piece, perfms in piece2perf.items() if piece in pieces_valid)
-        n_perfms_test = sum(len(perfms) for piece, perfms in piece2perf.items() if piece in pieces_test)
+        for _, row in self.metadata.iterrows():
+            if row['split'] == '--':
+                continue
+
+            if row['source'] == 'ASAP':
+                perfm_label = row['midi_perfm']
+            elif row['source'] == 'MAPS':
+                perfm_label = '_'.join(row['midi_perfm'].split('/')[-1][len('MAPS_MUS-'):].split('_')[:-1])
+            elif row['source'] == 'CPM':
+                perfm_label = row['midi_perfm'].split('/')[-1][:-4]
+            
+            if perfm_label not in piece2perfm_label[row['piece_id']]:
+                piece2perfm_label[row['piece_id']].add(perfm_label)
+                perfms[row['split']].add(row['performance_id'])
+            
+        perfms_all = perfms['train'] | perfms['valid'] | perfms['test']
+        n_perfms_train = len(perfms['train'])
+        n_perfms_valid = len(perfms['valid'])
+        n_perfms_test = len(perfms['test'])
         n_perfms = n_perfms_train + n_perfms_valid + n_perfms_test
 
-        # duration
-        
+        # ========== duration & number of notes ==========
+        self.verboseprint('INFO: Get duration & number of notes')
+
+        def cache_duration_n_notes(row):
+            if row['performance_id'] in perfms_all:
+                self.verboseprint('INFO: Get duration & number of notes for performance {}'.format(row['performance_id']))
+
+                midi_data = pm.PrettyMIDI(row['midi_perfm'])
+                duration = midi_data.get_end_time()
+                n_notes = np.sum([len(midi_data.instruments[i].notes) for i in range(len(midi_data.instruments))])
+
+                pickle.dump((duration, n_notes), open(str(Path(self.feature_folder, 'temp', row['performance_id']+'.pkl')), 'wb'))
+
+        Path(self.feature_folder, 'temp').mkdir(parents=True, exist_ok=True)
+        rows = [row for _, row in self.metadata.iterrows()]
+        pool = Pool(processes=self.workers)
+        pool.map(cache_duration_n_notes, rows)
+
+        duration_train, duration_valid, duration_test = 0, 0, 0
+        n_notes_train, n_notes_valid, n_notes_test = 0, 0, 0
+
+        for _, row in self.metadata.iterrows():
+            if row['performance_id'] in perfms_all:
+                duration, n_notes = pickle.load(open(str(Path(self.feature_folder, 'temp', row['feature_file'])), 'rb'))
+
+                if row['performance_id'] in perfms['train']:
+                    duration_train += duration
+                    n_notes_train += n_notes
+                elif row['performance_id'] in perfms['valid']:
+                    duration_valid += duration
+                    n_notes_valid += n_notes
+                elif row['performance_id'] in perfms['test']:
+                    duration_test += duration
+                    n_notes_test += n_notes
+        shutil.rmtree(str(Path(self.feature_folder, 'temp')))
+
+        duration_all = duration_train + duration_valid + duration_test
+        n_notes_all = n_notes_train + n_notes_valid + n_notes_test
 
         # ======== print dataset statistics ==========
-        self.verboseprint('\n\tNumber of distinct music pieces: {}'.format(n_pieces))
-        
+        self.verboseprint('\n\t=================== Dataset Statistics ====================')
+        self.verboseprint('\t\t\tTrain\t\tValid\t\tTest\t\tAll')
+        self.verboseprint('\tn_pieces:\t{}\t\t{}\t\t{}\t\t{}'.format(n_pieces_train, n_pieces_valid, n_pieces_test, n_pieces))
+        self.verboseprint('\tn_perfms:\t{}\t\t{}\t\t{}\t\t{}'.format(n_perfms_train, n_perfms_valid, n_perfms_test, n_perfms))
+        self.verboseprint('\tduration (h):\t{:.1f}\t\t{:.1f}\t\t{:.1f}\t\t{:.1f}'.format(duration_train/3600, duration_valid/3600, duration_test/3600, duration_all/3600))
+        self.verboseprint('\tn_notes (k):\t{:.1f}\t\t{:.1f}\t\t{:.1f}\t\t{:.1f}\n'.format(n_notes_train/1000, n_notes_valid/1000, n_notes_test/1000, n_notes_all/1000))
         
     def prepare_features(self):
         self.verboseprint('INFO: Preparing features...')
@@ -201,8 +253,7 @@ class FeaturePreparation():
             else:
                 note_sequence, beats = DataUtils.get_note_sequence_and_beats_from_midi(row['midi_perfm'])
 
-            feature = (note_sequence, beats)
-            pickle.dump(feature, open(row['feature_file'], 'wb'))
+            pickle.dump((note_sequence, beats), open(row['feature_file'], 'wb'))
 
         # prepare features with multiprocessing
         rows = [row for _, row in self.metadata.iterrows()]
